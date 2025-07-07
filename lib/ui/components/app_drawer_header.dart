c64c43d4fea28df68076c0241db68bb693769026
desenv <desenv@10.66.13.74>
2025-07-07 15:01:47 -0300

    Adicionado clarity mask em elementos sensíveis

--- lib/ui/components/app_drawer_header.dart
+++ lib/ui/components/app_drawer_header.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:patrimonio/app/providers/user_provider.dart';
+import 'package:clarity_flutter/clarity_flutter.dart';
 
 class AppDrawerHeader extends StatelessWidget {
   const AppDrawerHeader({super.key});
@@ -16,21 +17,27 @@ class AppDrawerHeader extends StatelessWidget {
             shape: BoxShape.circle,
             border: Border.all(color: Colors.white, width: 2),
           ),
-          child: const CircleAvatar(
-            radius: 40,
-            backgroundImage: AssetImage("assets/images/empregado.png"),
+          child: const ClarityMask(
+            child: CircleAvatar(
+              radius: 40,
+              backgroundImage: AssetImage("assets/images/empregado.png"),
+            ),
           ),
         ),
         const SizedBox(height: 20),
-        Text(
-          context.watch<UserProvider>().nome,
-          textAlign: TextAlign.center,
-          style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
+        ClarityMask(
+          child: Text(
+            context.watch<UserProvider>().nome,
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
+          ),
         ),
-        Text(
-          context.watch<UserProvider>().matricula,
-          textAlign: TextAlign.center,
-          style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
+        ClarityMask(
+          child: Text(
+            context.watch<UserProvider>().matricula,
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
+          ),
         ),
       ],
     );
