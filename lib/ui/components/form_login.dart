ea9383a58366e8cad91d205762000b0edaa1d9ca
desenv <desenv@10.66.13.74>
2025-07-10 15:34:18 -0300

    Adicionado o setCustomUserId para acompanhamento;

--- lib/ui/components/form_login.dart
+++ lib/ui/components/form_login.dart
@@ -43,6 +43,7 @@ class _FormLoginState extends State<FormLogin> {
       var loginStatus = jsonDecode(res);
       if (loginStatus['login_result']) {
         Clarity.sendCustomEvent(loginStatus['usuario']['permissao']);
+        Clarity.setCustomUserId(loginStatus['usuario']['matricula']);
         setState(() => isLoading = false);
         if (mounted) {
           context.read<UserProvider>().registraLogin(
