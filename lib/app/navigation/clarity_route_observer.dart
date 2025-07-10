877261d341ce80051fa5536bfd5f2db728f7e0d8
desenv <desenv@10.66.13.74>
2025-07-10 13:22:19 -0300

    Criado o route observer para reportar ao clarity em qual página o usuario está

---
+++ lib/app/navigation/clarity_route_observer.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:clarity_flutter/clarity_flutter.dart';
+
+// Route Observer criado para centralizar o envio do nome das telas ao Clarity
+// Evita a necessidade de incluir no initState da página, uma vez que sempre que
+// uma rota é acessada, o observer é automáticamente chamado.
+class ClarityRouteObserver extends RouteObserver<PageRoute<dynamic>> {
+  void _sendScreenName(Route? route) {
+    if (route is PageRoute) {
+      final screenName = route.settings.name ?? route.runtimeType.toString();
+      if (screenName.trim().isNotEmpty) {
+        debugPrint(
+          "\n###########################\n"
+          "Nome de página registrado no Micorsoft Clarity: $screenName"
+          " \n###########################\n",
+        );
+        Clarity.setCurrentScreenName(screenName);
+      }
+    }
+  }
+
+  @override
+  void didPush(Route route, Route? previousRoute) {
+    super.didPush(route, previousRoute);
+    _sendScreenName(route);
+  }
+
+  @override
+  void didReplace({Route? newRoute, Route? oldRoute}) {
+    super.didReplace(newRoute: newRoute, oldRoute: oldRoute);
+    _sendScreenName(newRoute);
+  }
+
+  @override
+  void didPop(Route route, Route? previousRoute) {
+    super.didPop(route, previousRoute);
+    _sendScreenName(previousRoute);
+  }
+}
