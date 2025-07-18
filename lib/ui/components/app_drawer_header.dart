80383cf3ed3c6681590c3ba07e2a05019e5e2fbb
desenv <desenv@10.66.13.74>
2025-07-18 18:48:54 -0300

    Incluída a busca por imagens com base no login do usuário

--- lib/ui/components/app_drawer_header.dart
+++ lib/ui/components/app_drawer_header.dart
@@ -3,8 +3,27 @@ import 'package:provider/provider.dart';
 import 'package:patrimonio/app/providers/user_provider.dart';
 import 'package:clarity_flutter/clarity_flutter.dart';
 
-class AppDrawerHeader extends StatelessWidget {
-  const AppDrawerHeader({super.key});
+class AppDrawerHeader extends StatefulWidget {
+  AppDrawerHeader({super.key});
+  static const String _placeholderImage = "assets/images/empregado_250x250.png";
+
+  @override
+  State<AppDrawerHeader> createState() => _AppDrawerHeaderState();
+}
+
+class _AppDrawerHeaderState extends State<AppDrawerHeader> {
+  late final String _userImage;
+
+  String _nomeFoto(String matricula) {
+    return matricula.substring(matricula.length - 7);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    context.read<UserProvider>().matricula;
+    _userImage = _nomeFoto("00000${context.read<UserProvider>().matricula}");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,11 +36,47 @@ class AppDrawerHeader extends StatelessWidget {
             shape: BoxShape.circle,
             border: Border.all(color: Colors.white, width: 2),
           ),
-          child: const ClarityUnmask(
+          child: ClarityUnmask(
             child: CircleAvatar(
               radius: 40,
-              backgroundImage: AssetImage(
-                "assets/images/empregado_250x250.png",
+              backgroundColor: Colors.grey,
+              child: ClipOval(
+                // Busca o formato JPEG
+                child: Image.network(
+                  'http://sgradsv.metro.df.gov.br:9666/$_userImage.jpeg',
+                  width: 80,
+                  height: 80,
+                  fit: BoxFit.cover,
+                  errorBuilder: (context, error, stackTrace) {
+                    return Image.network(
+                      'http://sgradsv.metro.df.gov.br:9666/$_userImage.jpg',
+                      width: 80,
+                      height: 80,
+                      fit: BoxFit.cover,
+                      errorBuilder: (context, error, stackTrace) {
+                        // Em caso de erro, retorna uma imagem local
+                        return Image.asset(
+                          AppDrawerHeader._placeholderImage,
+                          width: 80,
+                          height: 80,
+                          fit: BoxFit.cover,
+                        );
+                      },
+                      loadingBuilder: (context, child, loadingProgress) {
+                        if (loadingProgress == null) return child;
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      },
+                    );
+                  },
+                  loadingBuilder: (context, child, loadingProgress) {
+                    if (loadingProgress == null) return child;
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  },
+                ),
               ),
             ),
           ),
